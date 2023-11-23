4ad856cd66974479de4b013681815b2c844d6951
steingartlab <steingartlab@gmail.com>
2023-11-22 21:20:51 -0500

    naming

--- botoros/config.py
+++ botoros/config.py
@@ -1,6 +1,6 @@
 BASE_URL = "https://order.dostoros.com"
-BROADWAY_URL = 'location/650926?fulfillment=pickup&lat=40.8093856&lng=-73.9600253'
-URL = f'{BASE_URL}/{BROADWAY_URL}'
+BROADWAY_URL = "location/650926?fulfillment=pickup&lat=40.8093856&lng=-73.9600253"
+URL = f"{BASE_URL}/{BROADWAY_URL}"
 TOPPINGS = (
     "Taqueria Salsa",
     "Corn Salsa",
@@ -12,13 +12,13 @@ TOPPINGS = (
     "Crumbled Queso Fresco",
     "Pickled Red Onions",
     "Spicy Avocado Goddess Dressing",
-    "Creamy Chipotle Dressing"
+    "Creamy Chipotle Dressing",
 )
-BASE_EMAIL = 'graphiteguru_INTEGER@gmail.com'
-RANDOM_NUMBER_URL = 'https://www.random.org/integers/?num=1&min=0&max=99999999&col=1&base=10&format=plain&rnd=new'
+BASE_EMAIL = "graphiteguru_INTEGER@gmail.com"
+RANDOM_NUMBER_URL = "https://www.random.org/integers/?num=1&min=0&max=99999999&col=1&base=10&format=plain&rnd=new"
 USER = {
-    'First name': 'Send',
-    'Last name': 'Nudes',
+    'First name': 'Gunnar',
+    'Last name': 'Thorsteinsson',
     'Phone': '212-854-1754',
     'Password': 'frank'
 }
